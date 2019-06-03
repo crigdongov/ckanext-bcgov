91d8f44d21b99fb9985ad62840550a0484d5c83a
Leo <leo.lou@gov.bc.ca>
2019-06-03 16:34:42 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ checkup.config = {
 	"timeframe": 1 * time.Day,
 	"refresh_interval": 60,
 	"storage": {
-		"url": "https://bcdc-checkup.data.gov.bc.ca"
+		"url": "https://gh-dbcrss.data.gov.bc.ca"
 	},
 	"status_text": {
 		"healthy": "Service Up",
